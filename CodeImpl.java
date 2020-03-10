ec617a1c34d0564b38147ace4bfd0a20af39d706
Eric Gumba <ericgumba@gmail.com>
2020-03-09 23:42:40 -0700

    Update CodeImpl.java

--- CodeImpl.java
+++ CodeImpl.java
@@ -1,15 +1,42 @@
 import java.util.BitSet;
+import java.util.HashMap;
 
 public class CodeImpl implements Code {
 
     public CodeImpl(){
 
+        HashMap<String, String> dest = new HashMap<String, String>();
+        dest.put("A", "100");
+        dest.put("M", "001");
+        dest.put("D", "010");
+        
+
+        HashMap<String, String> compA = new HashMap<String, String>();
+        compA.put("M", " 110000");
+        compA.put("!M",  "110001");
+        compA.put("-M",  "110011");
+        compA.put("M+1", "110111");
+        compA.put("M-1", "110010");
+        compA.put("D+M", "000010");
+        compA.put("D-M",  "010011");
+        compA.put("M-D", "000111");
+        compA.put("D&M", "000000");
+        compA.put("D|M", "010101");
+
+        HashMap<String, String> dest = new HashMap<String, String>();
+        dest.put("A", "");
+        dest.put("M", "");
+        dest.put("D", "");
     }
 
     @Override
     public BitSet dest(String mnemonic){
 
+
         BitSet bs = new BitSet(3); 
+        
+
+        
         return null;
     }
 
